316adbb21affac35ff6de86dca1bc8d24650f7c5
Juan Avila <dev@juanavila.es>
2020-11-09 12:16:16 +0100

    Fix board builder

--- src/test/java/usantatecla/draughts/models/BoardBuilder.java
+++ src/test/java/usantatecla/draughts/models/BoardBuilder.java
@@ -23,13 +23,23 @@ public class BoardBuilder {
 
         Board board = new Board();
         for(int i = 0; i < Coordinate.getDimension(); i++) {
+            String row = rows.get(i);
             for(int j = 0; j < Coordinate.getDimension(); j++) {
+                char cell = row.charAt(j);
                 Coordinate coordinate = new Coordinate(i,j);
-                if(coordinate.isBlack() && (Color.BLACK.isInitialRow(i) || Color.WHITE.isInitialRow(i)))
-                    board.put(coordinate, new Pawn(Color.getInitialColor(coordinate)));
+                if(coordinate.isBlack())
+                    board.put(coordinate, pieceOf(cell));
             }
         }
         return board;
+    }
+
+    private Piece pieceOf(char cell) {
+        if(cell == 'b') return new Pawn(Color.WHITE);
+        if(cell == 'B') return new Draught(Color.WHITE);
+        if(cell == 'n') return new Pawn(Color.BLACK);
+        if(cell == 'N') return new Draught(Color.BLACK);
+        return null;
     }
 
 }
